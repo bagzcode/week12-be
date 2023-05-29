0cfb0fb26f49de8c33d4c80ce651ff1122e3141a
bagzcode <bagus.manuaba@gmail.com>
2023-05-29 23:20:10 +0700

    add db

--- server/api.py
+++ server/api.py
@@ -1,7 +1,18 @@
 from server.routes import router as TodoRouter
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["https://front-end-steel-phi.vercel.app/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["Root"])
